d3c0535d66682db96b2f66365540fcdd67f7c360
logcos2x <soumikpaul5y@gmail.com>
2024-10-26 23:51:40 +0530

    Create stone_papers_scissor.c

---
+++ stone_papers_scissor.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int getcmp()
+{
+    int x=rand()%3+1;
+    return x;
+}
+void show(int p, int cmp, int *c1, int *c2)
+{
+    printf("You Chose: %d\tComputer Chose: %d\n",p,cmp);
+    if(p==cmp)
+    {
+        printf("It's a draw :(\n");
+    }
+    else if( p==2 && cmp==1 || p==1 && cmp==3 || p==3 && cmp ==1)
+    {
+        printf("You win this round!\n");
+        (*c1)++;
+    }
+    else
+    {
+        printf("Computer wins this round\n");
+        (*c2)++;
+    }
+    printf("Your Score: %d\tComputer: %d\n", *c1, *c2);
+}
+int main(){
+    int p,c1=0,c2=0;
+    srand(time(0));
+    printf("Welcome to the game!\n");
+    do{
+        printf("1)Stone\n2)Paper\n3)Scissor\n0)Exit\nEnter your choice: ");
+        scanf("%d", &p);
+        if(p==0){
+        break;
+        }
+        else if(p>=1 && p<=3){
+        show(p, getcmp(),&c1,&c2);
+        }
+        else{
+        printf("Invalid Choice\n");
+        }
+    }
+    while(p>=1 && p<=3);
+    printf("Thanks for playing :)");
+    return 0;
+}
