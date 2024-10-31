fdc55a1371bf7077095640ca6d7a1beda2fc0b37
logcos2x <soumikpaul5y@gmail.com>
2024-10-31 12:55:58 +0530

    Update bank.c

--- bank.c
+++ bank.c
@@ -1,30 +1,65 @@
 #include <stdio.h>
 #include <string.h>
-
-typedef struct bank{
+#include <stdlib.h>
+#include <time.h>
+typedef struct bank
+{
     char name[35];
-    int acc_no;
+    double acc_no;
     float balance;
-}bk;
+} bk;
+double ifc()
+{
+    double code = 0, ran;
+    srand(time(0) + rand());
+    for (int i = 0; i < 11; i++)
+    {
+        ran = rand() % 10;
+        code = code * 10 + ran;
+    }
+    return code;
+}
 void cr_acc()
 {
-    bk user;
-    FILE *ptr=fopen("details.txt","a");
-    if(ptr==NULL){
-        printf("\nServer Error 404 :(");
+    bk user, read;
+    FILE *ptr = fopen("details.txt", "a+");
+    if (ptr == NULL)
+    {
+        printf("\nServer Error 404 :(\n");
+        return;
     }
+
     char c;
-    do{
-        c=getchar();
-    }while(c!='\n' && c!=EOF);
+    do
+    {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+
     printf("\nCreating an account...\n");
     printf("\nEnter your name: ");
-    fgets(user.name, sizeof(user.name),stdin);
-    user.name[strcspn(user.name, "\n")]='\0';
-    printf("Enter your account  number: ");
-    scanf("%d", &user.acc_no);
-    user.balance=0;
-    fwrite(&user,sizeof(user),1, ptr);
+    fgets(user.name, sizeof(user.name), stdin);
+    user.name[strcspn(user.name, "\n")] = '\0';
+
+    int unique = 0;
+    while (!unique)
+    {
+        user.acc_no = ifc();
+        unique = 1;
+
+        rewind(ptr);
+        while (fread(&read, sizeof(read), 1, ptr))
+        {
+            if (user.acc_no == read.acc_no)
+            {
+                unique = 0;
+                break;
+            }
+        }
+    }
+
+    printf("\nYour generated account number: %0.lf\n", user.acc_no);
+    user.balance = 0;
+    fwrite(&user, sizeof(user), 1, ptr);
     fclose(ptr);
     printf("\nAccount Created Successfully!\n");
 }
@@ -38,33 +73,35 @@ void withdraw_m()
 }
 void check_b()
 {
-     bk read;
-     int comp;
-    FILE *ptr=fopen("details.txt","a");
-    if(ptr==NULL){
+    bk read;
+    double comp;
+    FILE *ptr = fopen("details.txt", "r");
+    if (ptr == NULL)
+    {
         printf("\nServer Error 404 :(");
     }
     printf("\nChecking Balance...\n");
     printf("\nEnter account number: ");
-    scanf("%d", &comp);
-    while(fread(&read, sizeof(read), 1, ptr))
+    scanf("%lf", &comp);
+    while (fread(&read, sizeof(read), 1, ptr))
     {
-        if(read.acc_no==comp){
-        printf("\nYour Account Balance is %.2f\n", read.balance);
-        fclose(ptr);
-        return;
+        if (read.acc_no == comp)
+        {
+            printf("\nYour Account Balance is %.2f\n", read.balance);
+            fclose(ptr);
+            return;
         }
     }
-        fclose(ptr);
-        printf("\nAccount number %d was not found :(\n",comp);
+    fclose(ptr);
+    printf("\nAccount number %.0lf was not found :(\n", comp);
 }
 int main()
 {
     int ch;
     while (1)
     {
-    printf("\n**Welcome to Bank Management System**\n\t1. Create Account\n\t2. Deposit Money\n\t3. Withdraw Money\n\t4. Check Balance\n\t5. Exit\nEnter Your Choice: ");
-    scanf("%d", &ch);
+        printf("\n**Welcome to Bank Management System**\n\t1. Create Account\n\t2. Deposit Money\n\t3. Withdraw Money\n\t4. Check Balance\n\t5. Exit\nEnter Your Choice: ");
+        scanf("%d", &ch);
         switch (ch)
         {
         case 1:
